e8903719f82f012c66589d9a9a5599ba69d9e9cb
PVm116rus <fire_ra@bk.ru>
2024-06-06 10:03:10 +0300

    integration UI tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-//    testImplementation("junit:junit:4.13.2")
-//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
@@ -79,10 +77,9 @@ dependencies {
     // Kotlin Extensions and Coroutines support for Room
     implementation ("androidx.room:room-ktx:2.6.1")
 
-
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
     implementation("com.google.dagger:hilt-android:2.48")
-//    ksp("com.google.dagger:dagger-compiler:2.51.1") // Dagger compiler
+//    ksp("com.google.dagger:dagger-compiler:2.48") // Dagger compiler
     ksp("com.google.dagger:hilt-compiler:2.48")   // Hilt compiler
     ksp("androidx.hilt:hilt-compiler:1.2.0")
 
@@ -110,8 +107,3 @@ dependencies {
 //    androidTestImplementation ("io.mockk:mockk-android:1.10.5")
     androidTestImplementation ("androidx.test:runner:1.4.0")
 }
-
-// Allow references to generated code
-//kapt {
-//    correctErrorTypes = true
-//}
